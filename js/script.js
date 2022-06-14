54d4c5f5a9f8fb818bd87dd145ae543185b7bae9
Turki-Sultan <107464004+Turki-Sultan@users.noreply.github.com>
2022-06-14 10:02:41 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var pra1=document.querySelector('#pra1');
+var pra2=document.querySelector('#pra2');
+var pra3=document.querySelector('#pra3');
+var pra4=document.querySelector('#pra4');
+var pra5=document.querySelector('#pra5');
+var pra6=document.querySelector('#pra6');
+var pra7=document.querySelector('#pra7');
+var pra8=document.querySelector('#pra8');
+
+
+function func2() {
+   pra2.classList.toggle("torki")
+}
+function func3() {
+   pra3.classList.toggle("torki")
+}
+function func4() {
+   pra4.classList.toggle("torki")
+}
+function func5() {
+   pra5.classList.toggle("torki")
+}
+function func6() {
+   pra6.classList.toggle("torki")
+}
+function func7() {
+   pra7.classList.toggle("torki")
+}
+function func8() {
+   pra8.classList.toggle("torki")
+}
+
+ console.log("%c ماذا تريد ياصديقي لا تحاول ان تعبث بالموقع","color:red") 
